b2d52954d0975218c8e35d34f307feb8a1d2a067
Matt Rozinskas <matt_rozinskas1@baylor.edu>
2022-02-16 15:46:28 -0600

    Changes made in class 2/16

--- Fly.py
+++ Fly.py
@@ -7,14 +7,14 @@ def main():
         I.Insect()
     )  # this creates an instance called 'my_coin' of the class 'Coin()'
 
-    mosquito = I.Insect
-    housefly = I.Insect
+    mosquito = I.Insect()
+    housefly = I.Insect()
 
-    mosquito.Length_of_flight
-    housefly.Length_of_flight
+    mosquito.Length_of_flight()
+    housefly.Length_of_flight()
 
-    print("The Mosquito can fly ", mosquito.get_flight, "miles")
-    print("The housefly can fly ", housefly.get_flight, "miles")
+    print("The Mosquito can fly ", mosquito.get_flight(), "miles")
+    print("The housefly can fly ", housefly.get_flight(), "miles")
 
 
 main()
